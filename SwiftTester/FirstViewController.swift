9b868ae63dc8d3d080703a1bba4fe6ad34bfe288
Andrew Smiley <Andrew@Andrews-MacBook-Pro.local>
2015-09-01 12:18:03 -0400

    Initial Commit

---
+++ SwiftTester/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftTester
+//
+//  Created by Andrew Smiley on 9/1/15.
+//  Copyright (c) 2015 Andrew Smiley. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
